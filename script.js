53c0f2cfa4809bf2022ce6dc3e269e1c49c90907
Carlos Adolfo <adolfo.ggc@gmail.com>
2018-12-15 22:38:40 -0300

    menu-area inserted. now floating areas

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 
 
-$(document).ready(function() {
-	new_width = $(window).width() - $(window).height();
-
-	$(".lateral").css("width", new_width);
+$(function() {
+	fit_screen();
 }) ;
+
+
+$(window).on('resize', function(){
+	fit_screen();
+});
+
+function fit_screen() {
+	new_width = $(window).width() - $(window).height();
+	$(".lateral").css("width", new_width);
+	$(".menu-area").css("width", new_width);
+};
